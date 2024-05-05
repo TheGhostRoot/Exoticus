0e198e8db2db544c122882963d4bba114fc847e7
Felix <fxp.8sam@icloud.com>
2024-05-05 13:41:51 +0200

    First version Error ext.
    Added a first version for the error extension. (not tested)

--- bot/extensions/error.py
+++ bot/extensions/error.py
@@ -1,5 +1,6 @@
 import hikari
 import lightbulb
+from pypika import Query, Table, Field
 
 
 plugin = lightbulb.Plugin("error")
@@ -16,4 +17,16 @@ def unload(bot):
     bot_obj = bot
 
 
-# Starting error handling
+# Starting error handling || This doesnt Work yet
+@plugin.listener(hikari.ExceptionEvent)
+async def on_error(event: hikari.ExceptionEvent):
+    
+    if event.failed_event is None:
+        sector = Schema('sector')
+        query = Query.from_(sector).select(sector.system.log_channel_id).where(sector.system.guild_id == guild_id)
+        result = await bot_obj.chacheManager.cached_dbselect(query)
+        error_channel_id = result[0][0]
+        error_channel = bot_obj.get_channel(error_channel_id)
+        ref = await record_error(event.exception, event.exc_info, event.failed_event)
+        await error_channel.send(f"{event.failed_event} Something went wrong (ref: {ref}).")
+    print(event.failed_event)
